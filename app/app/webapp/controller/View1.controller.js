bb03efb9121c26d635531db88843683762b85a6a
malyala manoj kumar <malyalamanojkumar@gmail.com>
2025-04-22 10:41:56 +0000

    added delete

--- app/app/webapp/controller/View1.controller.js
+++ app/app/webapp/controller/View1.controller.js
@@ -71,5 +71,52 @@ $.ajax({
             });
             
         },
+        onUpdatePress(oEvent){
+           var oItem = oEvent.getSource().getParent();
+           var oContext = oItem.getBindingContext("mainModel");
+           var oDialog = this.byId("updateEmployeeDialog");
+          oDialog.setBindingContext(oContext, "mainModel");
+          oDialog.open();
+        },
+        onCancelUpdateEmployee(){
+            this.byId("updateEmployeeDialog").close();
+        },
+        onConfirmUpdateEmployee() {
+            // Logic to update an employee
+            var sID = this.byId("u_employeeID").getValue().replace(/,/g, '');
+            var sFirstName = this.byId("u_employeeFirstName").getValue();
+            var sLastName = this.byId("u_employeeLastName").getValue();
+            var sJobTitle = this.byId("u_employeeJobTitle").getValue();
+            var sCompanyID = this.byId("u_employeeCompanyID").getValue().replace(/,/g, '');
+            var sSalary = this.byId("u_employeeSalary").getValue().replace(/,/g, ''); 
+        
+            // Create the updated employee object
+            var oUpdatedEmployee = {
+                ID: sID,
+                firstName: sFirstName,
+                lastName: sLastName,
+                jobTitle: sJobTitle,
+                companyId_ID: sCompanyID,
+                salary: sSalary
+            };
+        
+            // Send a PUT request to the CAP service to update the employee
+            $.ajax({
+                url: "/odata/v4/my/employee(" + sID + ")",
+                method: "PUT",
+                contentType: "application/json",
+                data: JSON.stringify(oUpdatedEmployee),
+                success: (data) => {
+                    MessageToast.show("Employee updated successfully!");
+                    this.byId("updateEmployeeDialog").close();
+                    // Optionally, refresh the model to update the table
+                    this.getView().getModel("mainModel").refresh();
+                },
+                error: (error) => {
+                    MessageToast.show("Error updating employee: " + error.responseText);
+                }
+            });
+        }
+        
     });
 });
